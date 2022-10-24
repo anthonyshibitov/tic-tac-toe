29dece8180483e8b922cdd35ca60ae1f375d7430
Anthony Shibitov <anthonyshibitov@gmail.com>
2022-10-24 19:12:35 -0400

    Add main gameboard logic and win logic

--- source.js
+++ source.js
@@ -6,16 +6,20 @@ const O_STATE = 2;
 const playerFactory = function(name_p, state_p) {
     let name = name_p;
     let state = state_p;
-    const sayName = () => {console.log(name)};
+    const getName = () => name;
+    const setName = (n) => {name = n};
+    const getState = () => state;
     return {
-        sayName
+        getName,
+        setName,
+        getState
     }
 }
 
 const Gameboard = (function() {
     // Create player objects
-    const pOne = playerFactory('bob', X_STATE);
-    const pTwo = playerFactory('jim', O_STATE);
+    const pOne = playerFactory('pOne', X_STATE);
+    const pTwo = playerFactory('pTwo', O_STATE);
 
     // Initialize gameboard
     const board = [
@@ -24,19 +28,72 @@ const Gameboard = (function() {
         [0, 0, 0]
     ]
 
-    const getBoard = () => { return board };
+    let gameTurn = 0;
 
+    const getBoard = () => board;
+
+    // ~~ is double NOT operator, which removes all decimals to get an integer
     const updateBoard = (position, player) => {
+        if(board[~~(position / 3)][position % 3] != 0) {
+            console.log('ERROR: Spot taken');
+            return 1;
+        }
+        if(position >= 0 && position <= 8) {
+            board[~~(position / 3)][position % 3] = player.getState();
+        } else {
+            console.log('ERROR: Position out of bounds');
+        }
+    }
+
+    const init = () => {
+
+    }
+
+    const gameLoop = () => {
+
+    }
+
+    // There are 8 win conditions. 3 vertical, 3 horizontal, 2 diagonal
+    // Return player object on win
+    const checkWin = () => {
+        // Horizontal
+        if(board[0][0] == board[0][1] && board[0][1] == board[0][1]) return board[0][0] == pOne.getState() ? pOne : pTwo;
+        if(board[1][0] == board[1][1] && board[1][1] == board[1][2]) return board[1][0] == pOne.getState() ? pOne : pTwo;
+        if(board[2][0] == board[2][1] && board[2][1] == board[2][2]) return board[2][0] == pOne.getState() ? pOne : pTwo;
+
+        // Vertical
+        if(board[0][0] == board[1][0] && board[1][0] == board[2][0]) return board[0][0] == pOne.getState() ? pOne : pTwo;
+        if(board[0][1] == board[1][1] && board[1][1] == board[2][1]) return board[0][1] == pOne.getState() ? pOne : pTwo;
+        if(board[0][2] == board[1][2] && board[1][2] == board[2][2]) return board[0][2] == pOne.getState() ? pOne : pTwo;
+
+        // Diagonal
+        if(board[0][0] == board[1][1] && board[1][1] == board[2][2]) return board[0][0] == pOne.getState() ? pOne : pTwo;
+        if(board[2][0] == board[1][1] && board[1][1] == board[0][2]) return board[2][0] == pOne.getState() ? pOne : pTwo;
+
+        return {};
+    }
+
+    const reset = () => {
 
     }
 
     return {
         pOne,
         pTwo,
-        getBoard
+        getBoard,
+        updateBoard,
+        checkWin
     }
 })();
 
-Gameboard.pOne.sayName();
-Gameboard.pTwo.sayName();
+Gameboard.pOne.setName('anthony');
+Gameboard.pTwo.setName('sasha');
+
+Gameboard.updateBoard(3, Gameboard.pTwo);
+Gameboard.updateBoard(4, Gameboard.pTwo);
+Gameboard.updateBoard(5, Gameboard.pTwo);
+
+let winner = Gameboard.checkWin();
+console.log(winner.getName());
+
 console.log(Gameboard.getBoard());
