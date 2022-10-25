309d22c8197ba8ea9248eafc9f323294b6664794
Anthony Shibitov <anthonyshibitov@gmail.com>
2022-10-24 20:54:54 -0400

    Add basic game drawing

--- source.js
+++ source.js
@@ -28,7 +28,8 @@ const Gameboard = (function() {
         [0, 0, 0]
     ]
 
-    let gameTurn = 0;
+    let currentTurn = 0;
+    let currentPlayer = pOne;
 
     const getBoard = () => board;
 
@@ -49,51 +50,160 @@ const Gameboard = (function() {
 
     }
 
-    const gameLoop = () => {
+    const makeMove = (position) => {
+        if(updateBoard(position, currentPlayer) != 1){
+            currentTurn++;
+            drawState(position);
+            console.log(currentPlayer.getName());
 
+            // Swap to next player
+            currentPlayer == pOne ? currentPlayer = pTwo : currentPlayer = pOne;
+
+            let winningPlayer = checkWin();
+            if(winningPlayer != 1){
+                playerWin(winningPlayer);
+            } else {
+                if(currentTurn == 9) {
+                    playerDraw();
+                }
+            }       
+        }        
     }
 
     // There are 8 win conditions. 3 vertical, 3 horizontal, 2 diagonal
     // Return player object on win
     const checkWin = () => {
         // Horizontal
-        if(board[0][0] == board[0][1] && board[0][1] == board[0][1]) return board[0][0] == pOne.getState() ? pOne : pTwo;
-        if(board[1][0] == board[1][1] && board[1][1] == board[1][2]) return board[1][0] == pOne.getState() ? pOne : pTwo;
-        if(board[2][0] == board[2][1] && board[2][1] == board[2][2]) return board[2][0] == pOne.getState() ? pOne : pTwo;
+        if((board[0][0] == board[0][1] && board[0][1] == board[0][2]) && board[0][0] != 0) return board[0][0] == pOne.getState() ? pOne : pTwo;
+        if((board[1][0] == board[1][1] && board[1][1] == board[1][2]) && board[1][0] != 0) return board[1][0] == pOne.getState() ? pOne : pTwo;
+        if((board[2][0] == board[2][1] && board[2][1] == board[2][2]) && board[2][0] != 0) return board[2][0] == pOne.getState() ? pOne : pTwo;
 
         // Vertical
-        if(board[0][0] == board[1][0] && board[1][0] == board[2][0]) return board[0][0] == pOne.getState() ? pOne : pTwo;
-        if(board[0][1] == board[1][1] && board[1][1] == board[2][1]) return board[0][1] == pOne.getState() ? pOne : pTwo;
-        if(board[0][2] == board[1][2] && board[1][2] == board[2][2]) return board[0][2] == pOne.getState() ? pOne : pTwo;
+        if((board[0][0] == board[1][0] && board[1][0] == board[2][0]) && board[0][0] != 0) return board[0][0] == pOne.getState() ? pOne : pTwo;
+        if((board[0][1] == board[1][1] && board[1][1] == board[2][1]) && board[0][1] != 0) return board[0][1] == pOne.getState() ? pOne : pTwo;
+        if((board[0][2] == board[1][2] && board[1][2] == board[2][2]) && board[0][2] != 0) return board[0][2] == pOne.getState() ? pOne : pTwo;
 
         // Diagonal
-        if(board[0][0] == board[1][1] && board[1][1] == board[2][2]) return board[0][0] == pOne.getState() ? pOne : pTwo;
-        if(board[2][0] == board[1][1] && board[1][1] == board[0][2]) return board[2][0] == pOne.getState() ? pOne : pTwo;
+        if((board[0][0] == board[1][1] && board[1][1] == board[2][2]) && board[0][0] != 0) return board[0][0] == pOne.getState() ? pOne : pTwo;
+        if((board[2][0] == board[1][1] && board[1][1] == board[0][2]) && board[2][0] != 0) return board[2][0] == pOne.getState() ? pOne : pTwo;
 
-        return {};
+        return 1;
+    }
+
+    const playerWin = (player) => {
+        // Logic for when someone wins
+        let x = player.getName();
+        console.log(player.getName(), 'has won!!');
+    }
+
+    const playerDraw = () => {
+        console.log('current turn: ', currentTurn);
+        console.log('its a draw :(');
+    }
+
+    const drawState = (position) => {
+        const boxElement = document.querySelector(`[data-box="${position}"]`);
+        if(currentPlayer.getState() == X_STATE){
+            boxElement.innerHTML = 'X';
+        } else {
+            boxElement.innerHTML = "O";
+        }  
     }
 
     const reset = () => {
-
+        currentTurn = 0;
+        currentPlayer = pOne;
+        for(let i = 0; i < 9; i++){
+            board[~~(i / 3)][i % 3] = 0;
+            let boxElement = document.querySelector(`[data-box="${i}"]`);
+            boxElement.innerHTML = '';
+        }
     }
+
+    // Add event listeners
+    const boxZero = document.querySelector('[data-box="0"]');
+    boxZero.addEventListener('click', () => {
+        makeMove(0);
+    });
+    const boxOne = document.querySelector('[data-box="1"]');
+    boxOne.addEventListener('click', () => {
+        makeMove(1);
+    });
+    const boxTwo = document.querySelector('[data-box="2"]');
+    boxTwo.addEventListener('click', () => {
+        makeMove(2);
+    });
+    const boxThree = document.querySelector('[data-box="3"]');
+    boxThree.addEventListener('click', () => {
+        makeMove(3);
+    });
+    const boxFour = document.querySelector('[data-box="4"]');
+    boxFour.addEventListener('click', () => {
+        makeMove(4);
+    });
+    const boxFive = document.querySelector('[data-box="5"]');
+    boxFive.addEventListener('click', () => {
+        makeMove(5);
+    });
+    const boxSix = document.querySelector('[data-box="6"]');
+    boxSix.addEventListener('click', () => {
+        makeMove(6);
+    });
+    const boxSeven = document.querySelector('[data-box="7"]');
+    boxSeven.addEventListener('click', () => {
+        makeMove(7);
+    });
+    const boxEight = document.querySelector('[data-box="8"]');
+    boxEight.addEventListener('click', () => {
+        makeMove(8);
+    })
+
+    const resetBtn = document.querySelector('#reset-btn');
+    resetBtn.addEventListener('click', () => {
+        reset();
+    });
+
+    const playerOneBtn = document.querySelector('[data-name="1"]');
+    playerOneBtn.addEventListener('click', () => {
+        console.log('p 1');
+    });
+
+    const playerTwoBtn = document.querySelector('[data-name="2"]');
+    playerTwoBtn.addEventListener('click', () => {
+        console.log('p 2');
+    })
 
     return {
         pOne,
         pTwo,
-        getBoard,
-        updateBoard,
-        checkWin
+        getBoard
     }
 })();
 
 Gameboard.pOne.setName('anthony');
 Gameboard.pTwo.setName('sasha');
 
-Gameboard.updateBoard(3, Gameboard.pTwo);
-Gameboard.updateBoard(4, Gameboard.pTwo);
-Gameboard.updateBoard(5, Gameboard.pTwo);
+// Gameboard.updateBoard(3, Gameboard.pTwo);
+// Gameboard.updateBoard(4, Gameboard.pTwo);
+// Gameboard.updateBoard(5, Gameboard.pTwo);
 
-let winner = Gameboard.checkWin();
-console.log(winner.getName());
+// let winner = Gameboard.checkWin();
+// console.log(winner.getName());
+
+// console.log(Gameboard.getBoard());
+
+// DRAW CONDITION
+// Gameboard.makeMove(4); 
+// Gameboard.makeMove(2);
+// Gameboard.makeMove(5); 
+// Gameboard.makeMove(3);
+// Gameboard.makeMove(1);
+// Gameboard.makeMove(7);
+// Gameboard.makeMove(6);
+// Gameboard.makeMove(8);
+// Gameboard.makeMove(0);
+
+
+
 
 console.log(Gameboard.getBoard());
